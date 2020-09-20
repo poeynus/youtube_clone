b06f46de9c6509ee89c1599c9d1f8912ee3af86e
poeynus <68466791+poeynus@users.noreply.github.com>
2020-09-20 20:48:23 +0900

    make some pug and controls
    pug: make videoVlock mixin
    
    controls: divide get, post about join, upload, login
    
    Fix: routes /:id make error in HTML so remake by method

---
+++ db.js
@@ -0,0 +1,54 @@
+export const videos = [
+  {
+    id: 324393,
+    title: "Video awesome",
+    description: "This is something I love",
+    views: 24,
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 213213,
+      name: "poeynus",
+      email: "p@asd.com",
+    },
+  },
+  {
+    id: 232133,
+    title: "Video super",
+    description: "This is something I love",
+    views: 242,
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 213213,
+      name: "poeynus",
+      email: "p@asd.com",
+    },
+  },
+  {
+    id: 332393,
+    title: "Video good",
+    description: "This is something I love",
+    views: 54,
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 213213,
+      name: "poeynus",
+      email: "p@asd.com",
+    },
+  },
+  {
+    id: 766393,
+    title: "Video bye",
+    description: "This is something I love",
+    views: 124,
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 213213,
+      name: "poeynus",
+      email: "p@asd.com",
+    },
+  },
+];
