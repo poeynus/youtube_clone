e1c0aca6b8b48736f57f2c81a07e50a76e68bb9b
poeynus <68466791+poeynus@users.noreply.github.com>
2020-09-25 21:19:40 +0900

    use mongoose
    make videos db and express videos

--- db.js
+++ db.js
@@ -1,18 +1,18 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
-mongoose.connect(
-  "mongodb://localhost:27017/we-tube",
-  {
-    useNewUrlParser: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true
-  }
-);
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+});
 
 const db = mongoose.connection;
 
 const handleOpen = () => console.log("✅  Connected to DB");
-const handleError = error => console.log(`❌ Error on DB Connection:${error}`);
+const handleError = (error) =>
+  console.log(`❌ Error on DB Connection:${error}`);
 
 db.once("open", handleOpen);
 db.on("error", handleError);
--- init.js
+++ init.js
@@ -1,7 +1,11 @@
 import app from "./app";
 import "./db";
+import dotenv from "dotenv";
+import "./models/Video";
+import "./models/Comment";
+dotenv.config();
 
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 
 const handleListening = () =>
   console.log(`Listening on: http://localhost:${PORT}`);
